47015bab86ea6251e1e7e7a7c287f484558a9b2f
Boipelo Ngakane <boipelongakane16@gmail.com>
2025-03-09 20:27:26 +0200

    updated typewriter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ function displayPoem(response) {
         strings: response.data.answer,
         autoStart: true,
         delay: 1,
-        cursor: "" ,
       });
 } 
 
@@ -16,7 +15,7 @@ function generatePoem(event) {
     let apiKey="2046c535afeb092fo82f1d306d8a2b2t";
     let prompt=`User instructions are: Generate a poem about $(instructionsInput.value)`;
     let context=
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic Html. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic html. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("Generating Poem");
